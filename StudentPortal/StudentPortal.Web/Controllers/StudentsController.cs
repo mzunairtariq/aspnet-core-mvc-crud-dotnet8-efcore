6eb5719db3d8a19fce143730fb37585418fcf7b7
mzunairtariq <contact.zunair@gmail.com>
2024-04-17 16:50:26 +0500

    Added form creation with Validation, inforamtion storing into DB, and student list display."

---
+++ StudentPortal/StudentPortal.Web/Controllers/StudentsController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentPortal.Web.Data;
+using StudentPortal.Web.Models;
+using StudentPortal.Web.Models.Entities;
+
+namespace StudentPortal.Web.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public StudentsController(ApplicationDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        // GET: Students
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel)
+        {
+            var newStudent = new Students
+            {
+                Name = viewModel.Name,
+                Phone = viewModel.Phone,
+                Email = viewModel.Email,
+                Subscribed = viewModel.Subscribed
+            };
+
+            await _dbContext.AddAsync(newStudent);
+            await _dbContext.SaveChangesAsync();
+
+            return View();
+        }
+
+        // GET: Students List
+        public async Task<IActionResult> List()
+        {
+            var students = await _dbContext.Students.ToListAsync();
+            return View(students);
+        }
+
+        // GET: Students Edit
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var student = await _dbContext.Students.FindAsync(id);
+            return View(student);
+        }
+
+
+    }
+
+}
---
+++ StudentPortal/StudentPortal.Web/Models/AddStudentViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentPortal.Web.Models
+{
+    public class AddStudentViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        public bool Subscribed { get; set; }
+    }
+}
